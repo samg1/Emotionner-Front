89e105c2d1f48ad7853f128ffbfe88f25a10345c
CharlesAndrea <charles.andrea@correo.unimet.edu.ve>
2020-06-02 21:18:05 -0400

    meep

--- src/Components/Forms/user-registration.js
+++ src/Components/Forms/user-registration.js
@@ -1,148 +1,176 @@
 import {Button, Form, FormGroup, Label, Input} from 'reactstrap';
-import React, {Component} from 'react';
+//import React, {Component} from 'react';
+import React,{ useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser} from '@fortawesome/free-solid-svg-icons'
 import axios from 'axios'; 
+import { Link } from '@reach/router';
+
+const SignUp = () => {
+  const [name, setName] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthdate, setBirthdate] = useState("");
+  const [ocupation, setOcupation] = useState("");
+  const [premium, setPremium] = useState(null);
+  const [password, setPassword] = useState("");
 
 
+  const createUserWithEmailAndPasswordHandler = (event, email, password) => {
+    event.preventDefault();
+    setName("");
+    setLastname("");
+    setEmail("");
+    setBirthdate("");
+    setOcupation("");
+    setPremium(null);
+    setPassword("");
+  };
 
-class UserRegistration extends Component{
+  const onChangeHandler = event => {
+    const { name, value } = event.currentTarget;
+    if (name === "name") {
+      setName(value);
+    }else if (name === "lastname") {
+      setLastname(value);
+    }else if (name === "email") {
+      setEmail(value);
+    }else if ( name === "birthdate") {
+      setBirthdate(value);
+    }else if (name === "ocupation") {
+      setOcupation(value);
+    }else if (name === "premium") {
+      setPremium(null);
+    }else if (name === "password") {
+      setPassword(value);
+    }
+  };
 
-    constructor(props){
-        super(props);
-        this.state = {
-          campName: "",
-          campLastname: "",
-          campEmail:"",
-          campBdate:"",
-          campOcupation:"",
-          campPassword: "",
-          article: "",
-          emotion:""
-        }
+  sendSave = () => {
+
+    if (name === "") {
+      alert("Introduzca su nombre")
+    }
+    else if (!validateEmail(email)) {
+        alert("El correo ingresado no es valido")
+    }
+    else if (lastname === "") {
+       alert("Introduzca su apellido")
+    }
+    else if (birthdate  === "") {
+       alert("Introduzca su fecha de nacimiento")
+    }
+    else if (ocupation === "" || ocupation === "Ocupación") {
+       alert("Introduzca su ocupación")
+    }
+    else if (email === "") {
+        alert("Introduzca su correo electrónico")
+    }
+    else if (password === "") {
+        alert("Introduzca una contraseña")
+    }
+    else {
+ 
+      const url = "https://emotionner.herokuapp.com/users/createUser"
+
+      const datapost = {
+        name : name,
+        lastname : lastname,
+        email : email,
+        birthdate : birthdate,
+        ocupation  : ocupation,
+        premium : 'false',
+        password : password
       }
 
-    render(){
-        return(
-            <div className="container ">
-                <div className= 'row '>
-                    <div className='col-sm-9 col-md-7 col-lg-5 mx-auto'>
-                        <div className='card card-signin my-5'>
-                            <div className='card-body'>
-                                <div className='form-icon'>
-                                    <span ><FontAwesomeIcon icon={faUser} /></span>
-                                </div>
-                               <h5 className='card-title text-center text-uppercase'>Registrate</h5>
-                               <Form className='form-singin'>
-                                    <FormGroup>
-                                        <Label>Nombre</Label>
-                                        <Input className='form-control' type="text" placeholder="Introduzca su nombre " required
-                                        value={this.state.campName} onChange={(value)=> this.setState({campName:value.target.value})}></Input>
-                                    </FormGroup>
-                                    <FormGroup>
-                                        <Label>Apellido</Label>
-                                        <Input className='form-control' type="text" placeholder="Introduzca su apellido " required
-                                        value={this.state.campLastname} onChange={(value)=> this.setState({campLastname:value.target.value})}></Input>
-                                    </FormGroup>
-                                    <FormGroup>
-                                        <Label>Fecha de nacimiento</Label>
-                                        <Input className='form-control' type="date" placeholder="Introduzca su fecha de nacimiento " required
-                                        value={this.state.campBdate} onChange={(value)=> this.setState({campBdate:value.target.value})}></Input>
-                                    </FormGroup>
-                                    <FormGroup>
-                                        <Label>Ocupación</Label>
-                                        <Input type="select"  value={this.state.campOcupation} onChange={(value)=> this.setState({campOcupation:value.target.value})}>
-                                        <option value="">Ocupación</option>
-                                        <option>Estudiante</option>
-                                        <option>Trabajo a tiempo completo</option>
-                                        <option>Trabajo a medio tiempo</option>
-                                        <option>Freelancer</option>
-                                        </Input>
-                                    </FormGroup>
-                                    <FormGroup>
-                                        <Label>Correo Electronico</Label>
-                                        <Input className='form-control' type="email" placeholder="Introduzca su correo " required
-                                        value={this.state.campEmail} onChange={(value)=> this.setState({campEmail:value.target.value})}></Input>
-                                    </FormGroup>
-                                    <FormGroup>
-                                        <Label>Contraseña</Label>
-                                        <Input className='form-control' type="password" placeholder="Introduzca su contraseña" required
-                                        value={this.state.campPassword} onChange={(value)=> this.setState({campPassword:value.target.value})}></Input>
-                                    </FormGroup>
-                                    <button type='submit' className="btn btn-lg btn-block text-uppercase btn-light" style={{backgroundColor:'#b79ced'}} onClick={()=>this.sendSave()}>Registrarse</button>
-                            </Form>
-                            </div>
+      console.log(datapost)
+      
+      axios.post(url,datapost)
+      .then(response=>{
+        if (response.data.success===true) {
+          alert(response.data.message)
+          window.location.replace("https://emotionner.web.app/");
+        }
+        else {
+          alert(response.data.message)
+        }
+      }).catch(error=>{
+        alert(""+error)
+      })
+ 
+    }
+
+  }; 
+
+  function validateEmail(email) 
+  {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
+  return(
+    <div className="container ">
+        <div className= 'row '>
+            <div className='col-sm-9 col-md-7 col-lg-5 mx-auto'>
+                <div className='card card-signin my-5'>
+                    <div className='card-body'>
+                        <div className='form-icon'>
+                            <span ><FontAwesomeIcon icon={faUser} /></span>
                         </div>
+                       <h5 className='card-title text-center text-uppercase'>Registrate</h5>
+                       <Form className='form-singin'>
+                            <FormGroup>
+                                <Label>Nombre</Label>
+                                <Input className='form-control' type="text" placeholder="Introduzca su nombre " name="name" 
+                                value={name} onChange={event => onChangeHandler(event)}></Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Apellido</Label>
+                                <Input className='form-control' type="text" placeholder="Introduzca su apellido " name="lastname" 
+                                value={lastname} onChange={event => onChangeHandler(event)}></Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Fecha de nacimiento</Label>
+                                <Input className='form-control' type="date" placeholder="Introduzca su fecha de nacimiento " name="birthdate" 
+                                value={birthdate} onChange={event => onChangeHandler(event)}></Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Ocupación</Label>
+                                <Input type="select"  name="ocupation" value={ocupation} onChange={event => onChangeHandler(event)}>
+                                <option value="">Ocupación</option>
+                                <option>Estudiante</option>
+                                <option>Trabajo a tiempo completo</option>
+                                <option>Trabajo a medio tiempo</option>
+                                <option>Freelancer</option>
+                                </Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Correo Electronico</Label>
+                                <Input className='form-control' type="email" placeholder="Introduzca su correo " name = "email"
+                                value={email} onChange={event => onChangeHandler(event)}></Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Contraseña</Label>
+                                <Input className='form-control' type="password" placeholder="Introduzca su contraseña" name="password"
+                                value={password} onChange={event => onChangeHandler(event)}></Input>
+                            </FormGroup>
+                            <button type='submit' className="btn btn-lg btn-block text-uppercase btn-light" style={{backgroundColor:'#b79ced'}} onClick={()=>this.sendSave()} 
+                              onClick={event => {
+                                createUserWithEmailAndPasswordHandler(event, email, password);
+                              }}
+                            >
+                              Registrarse 
+                            </button>
+                    </Form>
                     </div>
                 </div>
             </div>
-            )
-        
-    }
+        </div>
+    </div>
+  )
 
-    sendSave(){
 
-        if (this.state.campName==="") {
-          alert("Introduzca su nombre")
-        }
-        else if (!validateEmail(this.state.campEmail)) {
-            alert("El correo ingresado no es valido")
-         }
-        else if (this.state.campLastname==="") {
-           alert("Introduzca su apellido")
-        }
-        else if (this.state.campBdate==="") {
-           alert("Introduzca su fecha de nacimiento")
-        }
-        else if (this.state.campOcupation=="" || this.state.campOcupation=="Ocupación") {
-           alert("Introduzca su ocupación")
-        }
-        else if (this.state.campEmail==="") {
-            alert("Introduzca su correo electrónico")
-         }
-        else if (this.state.campPassword==="") {
-            alert("Introduzca una contraseña")
-         }
-        else {
-     
-          const url = "https://emotionner.herokuapp.com/users/createUser"
+}; 
+export default SignUp;
 
-          const datapost = {
-            name : this.state.campName,
-            lastname : this.state.campLastname,
-            email : this.state.campEmail,
-            birthdate : this.state.campBdate,
-            ocupation  : this.state.campOcupation,
-            premium : 'false',
-            password : this.state.campPassword
-          }
 
-          console.log(datapost)
-          
-          axios.post(url,datapost)
-          .then(response=>{
-            if (response.data.success===true) {
-              alert(response.data.message)
-              window.location.replace("https://emotionner.web.app/");
-            }
-            else {
-              alert(response.data.message)
-            }
-          }).catch(error=>{
-            alert(""+error)
-          })
-     
-        }
-
-        }
-
-      
-    
-}
-export default UserRegistration
-
-function validateEmail(email) 
-    {
-        var re = /\S+@\S+\.\S+/;
-        return re.test(email);
-    }
