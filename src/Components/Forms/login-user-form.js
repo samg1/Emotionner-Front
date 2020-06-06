cd52a48227ce2217b5a51f7253bd02c0dc5c66e7
CharlesAndrea <charles.andrea@correo.unimet.edu.ve>
2020-06-06 15:51:11 -0400

    intento

--- src/Components/Forms/login-user-form.js
+++ src/Components/Forms/login-user-form.js
@@ -39,7 +39,7 @@ const Login = () => {
                     <FormGroup>
                         <label>
                             <span>Correo Electrónico</span>
-                            <input  type = "email" placeholder="Introduzca su correo"
+                            <input  type = "email" placeholder="Introduzca su correo" className="input-1 input-2"
                                 name = "userEmail"
                                 value = {email}
                                 id = "userEmail"
@@ -51,7 +51,7 @@ const Login = () => {
                     <FormGroup>
                         <label>
                             <span>Contraseña</span>
-                            <input  type = "password" placeholder = "Introduzca su contraseña"
+                            <input  type = "password" placeholder = "Introduzca su contraseña" className="input-1 input-2"
                                 name = "userPassword"
                                 value = {password}
                                 id = "userPassword"
