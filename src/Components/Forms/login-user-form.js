d3345fb8bc3bbd4b133dd03e8f4cce8fe8404155
CharlesAndrea <charles.andrea@correo.unimet.edu.ve>
2020-06-09 21:36:09 -0400

    sera que sirves????

--- src/Components/Forms/login-user-form.js
+++ src/Components/Forms/login-user-form.js
@@ -1,8 +1,9 @@
-import React, {useState} from "react";
-import {Button, Form, FormGroup, Label, Input} from 'reactstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser} from '@fortawesome/free-solid-svg-icons'
+import React, {useState, useContext} from "react";
+import {Form, FormGroup,Input} from 'reactstrap';
 import { auth, signInWithGoogle, generateUserDocument } from "../../firebase";
+import {AuthContext} from '../Authorization/Auth';
+import { withRouter, Redirect } from "react-router";
+//import Footer from '../Elements/footerOutside'
 
 
 const Login = () => {
@@ -29,6 +30,10 @@ const Login = () => {
             setPassword(value);
         }
     };
+    const {currentUser} = useContext(AuthContext);
+    if(currentUser){
+        return <Redirect to ='/calendar'/>
+    }
 
     return (
         <>
@@ -37,29 +42,29 @@ const Login = () => {
             <div className="form sign-in">
                 <h2>¡Bienvenido de Vuelta!</h2>
                 <Form>
-                    <FormGroup>
-                        <label>
-                            <span>Correo Electrónico</span>
-                            <Input  type = "email" placeholder="Introduzca su correo" className="input-1 input-2"
-                                name = "userEmail"
-                                value = {email}
-                                id = "userEmail"
-                                onChange = {(event) => onChangeHandler(event)}
-                                required
-                            ></Input>
-                        </label>
+                    <FormGroup className="label-1">
+            
+                        <span>Correo Electrónico</span>
+                        <Input  type = "email" placeholder="Introduzca su correo" className="input-1 input-2"
+                            name = "userEmail"
+                            value = {email}
+                            id = "userEmail"
+                            onChange = {(event) => onChangeHandler(event)}
+                            required
+                        ></Input>
+                        
                     </FormGroup>
-                    <FormGroup>
-                        <label>
-                            <span>Contraseña</span>
-                            <Input  type = "password" placeholder = "Introduzca su contraseña" className="input-1 input-2"
-                                name = "userPassword"
-                                value = {password}
-                                id = "userPassword"
-                                onChange = {(event) => onChangeHandler(event)}
-                                required
-                            ></Input>
-                        </label>
+                    <FormGroup className="label-1">
+            
+                        <span>Contraseña</span>
+                        <Input  type = "password" placeholder = "Introduzca su contraseña" className="input-1 input-2"
+                            name = "userPassword"
+                            value = {password}
+                            id = "userPassword"
+                            onChange = {(event) => onChangeHandler(event)}
+                            required
+                        ></Input>
+                        
                     </FormGroup>
                     <button  type="button" className="submit" style={{backgroundColor:'#b79ced'}} onClick = {(event) => {signInWithEmailAndPasswordHandler(event, email, password)}}>
                         Iniciar Sesión
@@ -85,93 +90,4 @@ const Login = () => {
         </>
     );
 }; 
-export default Login;
-
-
-
-
-
-
-
-
-
-
-/**
- const Login = () => {
-
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(null);
-
-    const signInWithEmailAndPasswordHandler = (event,email, password) => {
-        event.preventDefault();
-        auth.signInWithEmailAndPassword(email, password).catch(error => {
-        setError("Error al ingresar a la cuenta!");
-          console.error("Error al ingresar a la cuenta", error);
-        });
-      };
-      
-      const onChangeHandler = (event) => {
-          const {name, value} = event.currentTarget;
-        
-          if(name === 'userEmail') {
-              setEmail(value);
-          }
-          else if(name === 'userPassword'){
-            setPassword(value);
-          }
-      };
-   
-
-  return (
-    <div className= 'container'>
-            <div className= 'row align-items-center'>
-                <div className = 'col-sm-9 col-md-7 col-lg-5 mx-auto'>
-                <div className='card card-signin my-5'>
-                    <div className='card-body'>
-                        <div className='form-icon'>
-                            <span ><FontAwesomeIcon icon={faUser} /></span>
-                        </div>
-                            <h5 className='card-title text-center text-uppercase'>Iniciar Sesión</h5>
-                            <Form>
-                                    <FormGroup>
-                                        <Label>Correo Electronico</Label>
-                                        <Input className='form-control' type="email" placeholder="Introduzca su correo "
-                                            name="userEmail"
-                                            value = {email}
-                                            id="userEmail"
-                                            onChange = {(event) => onChangeHandler(event)}
-                                            required
-                                        ></Input>
-                                    </FormGroup>
-                                    <FormGroup>
-                                        <Label>Contraseña</Label>
-                                        <Input className='form-control' type="password" placeholder="Introduzca su contraseña" 
-                                            name="userPassword"
-                                            value = {password}
-                                            placeholder="Your Password"
-                                            id="userPassword"
-                                            onChange = {(event) => onChangeHandler(event)}
-                                        required></Input>
-                                    </FormGroup>
-                                    <div className="form-group">
-                                    <button className="btn btn-lg btn-block text-uppercase btn-light" style={{backgroundColor:'#b79ced'}} onClick = {(event) => {signInWithEmailAndPasswordHandler(event, email, password)}}>
-                                                  Iniciar Sesion
-                                    </button>
-                                    </div>
-                                    <div className="text-center">
-                                        <a href="/singUp">¿No tienes cuenta? Regístrate ahora</a>
-                                    </div>
-                            </Form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-  );
-};
-
-export default Login;
-*/
-
-
+export default withRouter(Login);
--- src/Components/Forms/user-registration.js
+++ src/Components/Forms/user-registration.js
@@ -4,7 +4,9 @@ import React,{ useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser} from '@fortawesome/free-solid-svg-icons'
 import axios from 'axios'; 
+import { withRouter } from "react-router";
 import { auth, signInWithGoogle, generateUserDocument } from "../../firebase";
+//import Footer from '../Elements/footerOutside';
 
 const SignUp = () => {
   const [name, setName] = useState("");
@@ -15,12 +17,26 @@ const SignUp = () => {
   const [premium, setPremium] = useState(null);
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+  
 
-  const createUserWithEmailAndPasswordHandler = async (event, email, password, name, lastname, birthdate, ocupation) => {
+  const createUserWithEmailAndPasswordHandler = async (event, email, password, name
+    , lastname, birthdate, ocupation) => {
     event.preventDefault();
+    var bd;
     try{
       const {user} = await auth.createUserWithEmailAndPassword(email, password);
+      console.log(user)
       generateUserDocument(user);
+      bd =true 
+      console.log(bd)
+    }
+    catch(error){
+      setError('Error Signing up with email and password');
+      bd= false
+      console.log(bd)
+    }
+
+    if(bd){
       const url = "https://emotionner.herokuapp.com/users/createUser"
 
       const datapost = {
@@ -47,14 +63,13 @@ const SignUp = () => {
       }).catch(error=>{
         alert(""+error)
       })
- 
-    }
-    catch(error){
-      setError('Error Signing up with email and password');
+      setEmail("");
+      setPassword("");
+    } else {
+      alert("Ha ocurrido un error al iniciar sesion")
     }
       
-    setEmail("");
-    setPassword("");
+    
   };
 
   const onChangeHandler = event => {
@@ -153,37 +168,34 @@ const SignUp = () => {
           <div className="form-reg sign-up">
           <h2 className="reg-h2">¡Regístrate ya!</h2>
             <Form>
-              <FormGroup>
-                <label>
-                  <span>Nombre</span>
-                  <Input type="text" className="input-1 input-2"
-                    placeholder="Introduzca su nombre" 
-                    name="name" 
-                    value={name} 
-                    onChange={event => onChangeHandler(event)}
-                  ></Input>
-                </label>
+              <FormGroup className="label-1">
+                <span>Nombre</span>
+                <Input type="text" className="input-1 input-2"
+                  placeholder="Introduzca su nombre" 
+                  name="name" 
+                  value={name} 
+                  onChange={event => onChangeHandler(event)}
+                ></Input>
               </FormGroup>
-              <FormGroup>
-                <label>
-                  <span>Apellido</span>
-                  <Input type="text" className="input-1 input-2"
-                    placeholder="Introduzca su apellido" 
-                   
-                  ></Input>
-                </label>
+              <FormGroup className="label-1">
+                <span>Apellido</span>
+                <Input type="text" className="input-1 input-2"
+                  placeholder="Introduzca su apellido" 
+                  lastname="lastname"
+                  value={lastname}
+                  onChange={event => onChangeHandler(event)}
+                ></Input>
               </FormGroup>
-              <FormGroup>
-                <label>
+              <FormGroup className="label-1">
                   <span>Correo Electrónico</span>
                   <Input type="email" className="input-1 input-2"
-                    placeholder="Introduzca su correo" 
-                  
+                    placeholder="Introduzca su correo"
+                    name="email"
+                    value={email}
+                    onChange={event => onChangeHandler(event)}
                   ></Input>
-                </label>
               </FormGroup>
-              <FormGroup>
-                <label>
+              <FormGroup className="label-1">
                   <span>Fecha de Nacimiento</span>
                   <Input type="date" className="input-1 input-2"
                     placeholder="Introduzca su fecha de nacimiento" 
@@ -191,10 +203,8 @@ const SignUp = () => {
                     value={birthdate} 
                     onChange={event => onChangeHandler(event)}
                   ></Input>
-                </label>
               </FormGroup>
-              <FormGroup>
-                <label>
+              <FormGroup className="label-1">
                   <span>Ocupación</span>
                   <Input type="select" name="ocupation" value={ocupation} onChange={event => onChangeHandler(event)} className="input-1 input-2">
                     <option value="">Ocupación</option>
@@ -203,10 +213,8 @@ const SignUp = () => {
                     <option>Trabajo a medio tiempo</option>
                     <option>Freelancer</option>
                   </Input>
-                </label>
               </FormGroup>
-              <FormGroup>
-                <label>
+              <FormGroup className="label-1">
                   <span>Contraseña</span>
                   <Input type="password" className="input-1 input-2"
                     placeholder="Introduzca su contraseña" 
@@ -214,7 +222,6 @@ const SignUp = () => {
                     value={password} 
                     onChange={event => onChangeHandler(event)}
                   ></Input>
-                </label>
               </FormGroup>
               <button type="button" className="submit" style={{backgroundColor:'#b79ced'}}
                 onClick={()=>sendSave()} 
@@ -232,172 +239,4 @@ const SignUp = () => {
     </>
   )
 };
-export default SignUp;
-
-/**<img alt="Logo" className="textt_img"></img> */
-/*
-const SignUp = () => {
-  const [name, setName] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [email, setEmail] = useState("");
-  const [birthdate, setBirthdate] = useState("");
-  const [ocupation, setOcupation] = useState("");
-  const [premium, setPremium] = useState(null);
-  const [password, setPassword] = useState("");
-
-
-  const createUserWithEmailAndPasswordHandler = (event, email, password) => {
-    event.preventDefault();
-    setName("");
-    setLastname("");
-    setEmail("");
-    setPassword("");
-  };
-
-  const onChangeHandler = event => {
-    const { name, value } = event.currentTarget;
-    if (name === "name") {
-      setName(value);
-    }else if (name === "lastname") {
-      setLastname(value);
-    }else if (name === "email") {
-      setEmail(value);
-    }else if ( name === "birthdate") {
-      setBirthdate(value);
-    }else if (name === "ocupation") {
-      setOcupation(value);
-    }else if (name === "premium") {
-      setPremium(null);
-    }else if (name === "password") {
-      setPassword(value);
-    }
-  };
-
-  const sendSave = () => {
-
-    if (name === "") {
-      alert("Introduzca su nombre")
-    }
-    else if (!validateEmail(email)) {
-        alert("El correo ingresado no es valido")
-    }
-    else if (lastname === "") {
-       alert("Introduzca su apellido")
-    }
-    else if (birthdate  === "") {
-       alert("Introduzca su fecha de nacimiento")
-    }
-    else if (ocupation === "" || ocupation === "Ocupación") {
-       alert("Introduzca su ocupación")
-    }
-    else if (email === "") {
-        alert("Introduzca su correo electrónico")
-    }
-    else if (password === "") {
-        alert("Introduzca una contraseña")
-    }
-    else {
- 
-      const url = "https://emotionner.herokuapp.com/users/createUser"
-
-      const datapost = {
-        name : name,
-        lastname : lastname,
-        email : email,
-        birthdate : birthdate,
-        ocupation  : ocupation,
-        premium : 'false',
-        password : password
-      }
-
-      console.log(datapost)
-      
-      axios.post(url,datapost)
-      .then(response=>{
-        if (response.data.success===true) {
-          alert(response.data.message)
-          window.location.replace("https://emotionner.web.app/");
-        }
-        else {
-          alert(response.data.message)
-        }
-      }).catch(error=>{
-        alert(""+error)
-      })
- 
-    }
-
-  }; 
-
-  function validateEmail(email) 
-  {
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-  }
-
-  return(
-    <div className="container ">
-        <div className= 'row '>
-            <div className='col-sm-9 col-md-7 col-lg-5 mx-auto'>
-                <div className='card card-signin my-5'>
-                    <div className='card-body'>
-                        <div className='form-icon'>
-                            <span ><FontAwesomeIcon icon={faUser} /></span>
-                        </div>
-                       <h5 className='card-title text-center text-uppercase'>Registrate</h5>
-                       <Form className='form-singin'>
-                            <FormGroup>
-                                <Label>Nombre</Label>
-                                <Input className='form-control' type="text" placeholder="Introduzca su nombre " name="name" 
-                                value={name} onChange={event => onChangeHandler(event)}></Input>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label>Apellido</Label>
-                                <Input className='form-control' type="text" placeholder="Introduzca su apellido " name="lastname" 
-                                value={lastname} onChange={event => onChangeHandler(event)}></Input>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label>Fecha de nacimiento</Label>
-                                <Input className='form-control' type="date" placeholder="Introduzca su fecha de nacimiento " name="birthdate" 
-                                value={birthdate} onChange={event => onChangeHandler(event)}></Input>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label>Ocupación</Label>
-                                <Input type="select"  name="ocupation" value={ocupation} onChange={event => onChangeHandler(event)}>
-                                <option value="">Ocupación</option>
-                                <option>Estudiante</option>
-                                <option>Trabajo a tiempo completo</option>
-                                <option>Trabajo a medio tiempo</option>
-                                <option>Freelancer</option>
-                                </Input>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label>Correo Electronico</Label>
-                                <Input className='form-control' type="email" placeholder="Introduzca su correo " name = "email"
-                                value={email} onChange={event => onChangeHandler(event)}></Input>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label>Contraseña</Label>
-                                <Input className='form-control' type="password" placeholder="Introduzca su contraseña" name="password"
-                                value={password} onChange={event => onChangeHandler(event)}></Input>
-                            </FormGroup>
-                            <button type='button' className="btn btn-lg btn-block text-uppercase btn-light" style={{backgroundColor:'#b79ced'}} onClick={()=>sendSave()} 
-                              onClick={event => {
-                                createUserWithEmailAndPasswordHandler(event, email, password, name
-                                  , lastname, birthdate, ocupation);
-                              }}
-                            >
-                              Registrarse 
-                            </button>
-                    </Form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
-
-
-}; 
-export default SignUp;
-*/
+export default withRouter(SignUp);
