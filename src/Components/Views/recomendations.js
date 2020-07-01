51cc6603df522b39e79ecad8abfbd4d170c9a228
Amanda Gonzalez <amandalug17@gmail.com>
2020-06-30 22:52:01 -0400

    arreglos

--- src/Components/Views/recomendations.js
+++ src/Components/Views/recomendations.js
@@ -13,42 +13,34 @@ class RecomendationView extends Component {
     return (
       <>
       <RecoHero/>
-      <Container>
-        <Row>
+      <Container >
+      <Row> 
           <Col>
-            <h1 className='d-flex justify-content-center'style={{margin: "20px", fontWeight:'lighter', letterSpacing: '5px', fontSize:'25px'}}>Recomendaciones según tus últimos registros.</h1>
+            <h1 className='d-flex justify-content-center'style={{margin: "20px", fontWeight:'lighter', letterSpacing: '5px', fontSize:'40px', fontFamily:'Montserrat'}}> Artículos recomendados</h1>
           </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h1 className='d-flex justify-content-start'style={{margin: "20px", fontFamily:'Dancing Script'}}>Artículos para ti</h1>
-          </Col>
-        </Row>
+      </Row>
         <Row>
           <ArticleView/>
         </Row>
         </Container>
-        
-            <div class="section-container">
-                <div class="left-banner"></div>
-                <div class="right-banner">
+        <Row style={{backgroundColor: '#b39bea'}}> 
+              <Col>
+                <h1 className='d-flex justify-content-center'style={{margin: "20px",fontFamily:'Montserrat', fontSize:'25px'}}> Tu frase de hoy</h1>
+              </Col>
+          </Row>
+          <Row style={{backgroundColor: '#b39bea'}}>
+              <PhraseView />
+          </Row>
+            <div className="section-container">
+                <div className="left-banner"></div>
+                <div className="right-banner">
                     <div>
                     <h3 style={{fontFamily:'Dancing Script', fontSize:'40px'}}>Tu espacio seguro</h3>
                     <p style={{fontFamily:'Montserrat'}}>¡Recuerda registrar todas tu tareas y emociones<br/>para recibir más recomendaciones como esta!</p>
                     </div>
                 </div>
             </div>
-        <Container>
-        <Row>
-          <Col>
-            <h1 className='d-flex justify-content-start'style={{margin: "20px", fontFamily:'Dancing Script'}}>Encuentra tu motivación</h1>
-          </Col>
-        </Row>
-        <Row>
-          <ArticleView/>
-        </Row>
-      </Container>
-      <Footer/>
+          <Footer/>
       </>
     )
   }
